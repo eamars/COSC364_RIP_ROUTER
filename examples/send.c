d7ad7d944416a381796f32bf21d7319ee44f231e
Ran Bao <worksev@gmail.com>
2015-04-21 00:08:44 +1200

    move from tcp to udp

--- examples/send.c
+++ examples/send.c
@@ -9,13 +9,37 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int read_from_client (int filedes)
+{
+	char buffer[24];
+	int nbytes;
+
+	nbytes = read (filedes, buffer, 24);
+	if (nbytes < 0)
+	{
+		/* Read error. */
+		perror ("Failed to read");
+		exit (-1);
+	}
+	else if (nbytes == 0)
+	/* End-of-file. */
+	{
+		return -1;
+	}
+	else
+	{
+		printf("recv: [%s]\n", buffer);
+		return 0;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	struct sockaddr_in server;
 	int socket_fd;
 	char *message;
 
-	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if (socket_fd < 0)
 	{
 		printf("Failed to create socket\n");
@@ -26,17 +50,19 @@ int main(int argc, char **argv)
 	server.sin_family = AF_INET;
 	server.sin_port = htons(atoi(argv[1]));
 
+	/*
 	// connect to remote server
 	if (connect(socket_fd, (struct sockaddr *)&server, sizeof(server)) < 0)
 	{
 		printf("Failed to connect to %s\n", argv[1]);
 		return -2;
 	}
+	*/
 
 	// send http header to remote host
 
 	message = argv[2];
-	if (send(socket_fd, message, strlen(message), 0) < 0)
+	if (sendto(socket_fd, message, strlen(message), 0, (struct sockaddr *)&server, sizeof(server)) < 0)
 	{
 		printf("Failed to send\n");
 		return -3;
@@ -44,14 +70,9 @@ int main(int argc, char **argv)
 	printf("send: [%s]\n", message);
 
 	// recv from remote
-	char reply[10240];
-	if (recv(socket_fd, reply, 10240, 0) < 0)
-	{
-		printf("Failed to receive\n");
-		return -4;
-	}
 
-	printf("recv: [%s]\n", reply);
+
+
 
 	close(socket_fd);
 
