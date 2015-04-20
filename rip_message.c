1eb3a71029b151de8b2e243e93b3538d3a18e908
Ran Bao <worksev@gmail.com>
2015-04-20 14:38:46 +1200

    check if the packet is valid

--- rip_message.c
+++ rip_message.c
@@ -8,19 +8,29 @@ static const char *RIP_PACKET_FORMAT = "%01x%01x00%02x00%04x0000%04x%04x";
 
 int rip_packet_decode(char *message, RIPPacket *packet)
 {
+	int recv_count;
+
 	if (strlen(message) != 24)
 	{
 		return -1;
 	}
 
-	return sscanf(message, RIP_PACKET_FORMAT,
+	recv_count = sscanf(message, RIP_PACKET_FORMAT,
 		&packet->command,
 		&packet->version,
 		&packet->AFI,
 		&packet->address,
 		&packet->next_hop,
 		&packet->metric
-	) != 6;
+	);
+
+	// check if metric <= 16
+	if (packet->metric > 16)
+	{
+		return -2;
+	}
+
+	return recv_count != 6;
 }
 
 
