bf3a87eeb85bbcf99b5225cfe2b40494a1e2c2fc
Ran Bao <worksev@gmail.com>
2015-04-22 00:25:17 +1200

    router is now able to exchange information

--- router_demon.c
+++ router_demon.c
@@ -7,19 +7,26 @@
 #include <string.h>
 #include <sys/select.h>
 #include <signal.h>
-
+#include <sys/time.h>
+#include <errno.h>
 
 #include "list.h"
 #include "routing_table.h"
 #include "rip_message.h"
 #include "pidlock.h"
 
+#define BUF_SZ 504 // max size for rip header + 25 * entry
+
+const int DEACTIVE_TIME = 10;
+const int GARBAGE_COLLECTION_TIME = 60;
+const int UPDATE_TIME = GARBAGE_COLLECTION_TIME / DEACTIVE_TIME;
+
 // global variable
 extern unsigned int router_id;
 extern char input_ports[512];
 extern char output_dest[512];
 
-#define BUF_SZ 504 // max size for rip header + 25 * entry
+static unsigned int second_tick;
 
 int make_socket(int port)
 {
@@ -47,40 +54,7 @@ int make_socket(int port)
 
 }
 
-int make_response(char *buf)
-{
-	// send routing tables to filedes (without split-horizon)
-	RIPPacket p;
-
-	int n = getNumEntry();
-	int hops[n];
-
-	// make rip packet
-	p.command = RIP_RESPONSE;
-	p.version = RIP_VERSION_2;
-	p.n_entry = n;
-
-	// prepare for send buffer
-	int send_size = 4 + n * 20;
-
-	// get router hops
-	// without split horizon
-	getAllHops(hops);
-	for (int i = 0; i < n; i++)
-	{
-		p.entry[i].AFI = AF_INET;
-		p.entry[i].address = router_id;
-		p.entry[i].next_hop = hops[i];
-		p.entry[i].metric = getValue(hops[i], METRIC);
-	}
-
-	// form raw string
-	rip_packet_encode(buf, &p);
-
-	return send_size;
-}
-
-int send_response(char *buf, int send_size, int sender_port)
+int send_message(char *buf, int send_size, int sender_port)
 {
 	struct sockaddr_in sock;
 	int sock_fd;
@@ -105,61 +79,202 @@ int send_response(char *buf, int send_size, int sender_port)
 	return 0;
 }
 
-int get_sender(char *buf)
+void make_response(void)
 {
-	RIPPacket packet;
-	rip_packet_decode(buf, &packet);
+	// send routing tables to filedes (without split-horizon)
+	RIPPacket p;
+
+	int sender_port;
+	int n = getNumEntry();
+	int hops[25];
+	int j;
 
 
-	return getValue(packet.entry[0].address, PORT);
+
+
+	// make rip packet
+	memset(&p, 0, sizeof(p));
+
+	p.command = RIP_RESPONSE;
+	p.version = RIP_VERSION_2;
+
+	// get router hops
+	// without split horizon
+	memset(hops, 0, sizeof(hops));
+	getAllHops(hops);
+
+	j = 0;
+	for (int i = 0; hops[i] != -1 && i < n; i++)
+	{
+		// ignore sending back the information about receiver
+		// this enables the split horizon
+		/*
+		if (hops[i] == receiver)
+		{
+			continue;
+		}
+		*/
+		p.entry[j].AFI = AF_INET;
+		p.entry[j].address = router_id;
+		p.entry[j].next_hop = hops[i];
+		p.entry[j].metric = getValue(hops[i], METRIC);
+
+		j++;
+	}
+
+
+	p.n_entry = j;
+
+	// prepare for send buffer
+	int send_size = 4 + j * 20;
+
+	// form raw string
+	char buf[send_size + 1];
+	memset(buf, 0, send_size);
+
+	rip_packet_encode(buf, &p);
+	printf("send: [%s]\n", buf);
+	// debug_print_rip_packet(&p);
+
+	// send to adjacent port
+	for (int i = 0; hops[i] != -1 && i < n; i++)
+	{
+		if ((sender_port = getValue(hops[i], PORT)) != 0 &&
+			getValue(hops[i], METRIC) != 16)
+		{
+			if (send_message(buf, send_size, sender_port) != 0)
+			{
+				perror("Failed to send");
+				exit(-1);
+			}
+			 printf("	to: %d [%d]\n", hops[i], sender_port);
+
+		}
+	}
+
 }
 
-int update_table(char *buffer)
+static void timer_handler()
 {
-	RIPPacket packet;
-	rip_packet_decode(buffer, &packet);
+	// atomic process, so disable the alarm at present
+	signal(SIGALRM, SIG_IGN);
+	timeoutUpdate();
+	printf("----------------\n");
+	printRoutingTable();
+	printf("----------------\n");
+
+	if (second_tick % UPDATE_TIME == 0)
+	{
+		make_response();
+	}
+
+
+	second_tick++;
+	// enable alarm again
+	signal(SIGALRM, timer_handler);
+}
+
+int update_table(RIPPacket *packet)
+{
+	int port;
+
+	debug_print_rip_packet(packet);
 
 	printf("Router%d\n", router_id);
 	printf ("Before: \n");
 	printRoutingTable();
-	for (unsigned int i = 0; i < packet.n_entry; i++)
+	for (unsigned int i = 0; i < packet->n_entry; i++)
 	{
+		/*
 		// split horizon enabled here
 		if (packet.entry[i].next_hop == router_id)
 		{
 			continue;
 		}
-		updateTable(
-			packet.entry[i].next_hop,
-			0,
-			packet.entry[i].address,
-			packet.entry[i].metric + getValue(packet.entry[i].address, METRIC)
-		);
+		*/
+		// if adjacent hop then update metric
+		port = getValue(packet->entry[i].next_hop, PORT);
+
+
+		// loop
+		if (packet->entry[i].next_hop == router_id)
+		{
+			updateTable(
+				packet->entry[i].address,
+				0,
+				packet->entry[i].address,
+				packet->entry[i].metric
+			);
+		}
+		// adjacent hop
+		else if (port != -1 && port != 0 && packet->entry[i].address != packet->entry[i].next_hop)
+		{
+			updateTable(
+				packet->entry[i].next_hop,
+				0,
+				packet->entry[i].address,
+				packet->entry[i].metric
+			);
+		}
+
+		// old hop awake
+		else if (getValue(packet->entry[i].address, METRIC) == 16)
+		{
+			updateTable(
+				packet->entry[i].next_hop,
+				0,
+				packet->entry[i].address,
+				packet->entry[i].metric
+			);
+		}
+
+
+		// metric = 16
+		else if (packet->entry[i].metric == 16)
+		{
+			updateTable(
+				packet->entry[i].next_hop,
+				0,
+				packet->entry[i].address,
+				16
+			);
+		}
+
+
+		// new hop
+		else
+		{
+			updateTable(
+				packet->entry[i].next_hop,
+				0,
+				packet->entry[i].address,
+				packet->entry[i].metric + getValue(packet->entry[i].address, METRIC)
+			);
+		}
+
 	}
 	printf ("After: \n");
 	printRoutingTable();
 	return 0;
 }
 
-int process_message(char *buffer)
+int decode_message(char *buffer, RIPPacket *p)
 {
 	int ret;
 
-	RIPPacket p;
 
 	// if successfully read from input ports, then parse the
 	// rip packet and add necessary information to routing
 	// table
-	if ((ret = rip_packet_decode(buffer, &p)) == 0)
+	if ((ret = rip_packet_decode(buffer, p)) == 0)
 	{
-		return p.command;
+		return p->command;
 	}
 	else
 	{
 		printf("Unable to parse[%d]\n", ret);
+		return ret;
 	}
-
-	return -1;
 }
 
 
@@ -168,6 +283,9 @@ int router_demon_start(void)
 {
 	/* Initilize */
 	int i;
+	int ret;
+	char buffer[BUF_SZ]; // raw buffer
+	struct itimerval timer;
 
 	// read and load listening ports from config file
 	SingleLinkedList inputs = split(input_ports, ',');
@@ -176,7 +294,8 @@ int router_demon_start(void)
 	SingleLinkedList outputs = split(output_dest, ',');
 
 	// get the number of input ports
-	int n_inputs = getLength(inputs);
+	int n_inputs = 0;
+	n_inputs = getLength(inputs);
 
 	// create a array to store these ports
 	int input_port_numbers[n_inputs];
@@ -187,13 +306,11 @@ int router_demon_start(void)
 		i++;
 	}
 
-	// index
-
-
 	// fds: file descriptor for listening ports
 	// server_fd: file descriptor that is ready to read
 	// client_fd: accepted connection
-	int fds[n_inputs], server_fd;
+	int fds[n_inputs];
+	int server_fd = 0;
 
 	// file descriptor set
 	fd_set readset;
@@ -205,20 +322,27 @@ int router_demon_start(void)
 	// client socket
 	struct sockaddr_in clientname;
 
-	int pid;
-	int ret;
-	char buffer[BUF_SZ]; // raw buffer
 
 	/* Process */
 
+
+	// set timer
+	timer.it_value.tv_sec = 1;
+	timer.it_value.tv_usec = 0; // starts almost immediately
+	timer.it_interval.tv_sec = 1; // period = 1Hz
+	timer.it_interval.tv_usec = 0;
+
+	// register timer
+	signal(SIGALRM, timer_handler);
+	// start timer
+	setitimer(ITIMER_REAL, &timer, NULL);
+
 	// create the initial routing with only adjacent routers
 	createRoutingTableFromConfig(outputs);
-	printRoutingTable();
 
 	// free the memory allocated by input and output
 	destroyList(inputs);
 	destroyList(outputs);
-
 
 
 	// create sockets for each of these input ports
@@ -228,32 +352,37 @@ int router_demon_start(void)
 		fds[i] = make_socket(input_port_numbers[i]);
 
 	}
-
-	// clear the read set
-	FD_ZERO(&readset);
-
-
 	// blocking
 	// Another example
 	// http://www.gnu.org/software/libc/manual/html_node/Server-Example.html
 	while (1)
 	{
 		// rebuild readset everytime after select
+		FD_ZERO(&readset);
 		for (i = 0; i < n_inputs; i++)
 		{
 			FD_SET(fds[i], &readset);
 		}
 
 		// select with socket is ready to read
-		if (select(FD_SETSIZE, &readset, NULL, NULL, NULL) < 0)
+		if ((ret = select(FD_SETSIZE, &readset, NULL, NULL, NULL)) < 0)
 		{
-			perror("Failed to select");
-			remove_pid(router_id);
-			exit(-1);
+			// select can catch the signal but we want to ignore it
+			if (errno == EINTR)
+			{
+				continue;
+			}
+			else
+			{
+				perror("Failed to select");
+				remove_pid(router_id);
+				exit(-1);
+			}
+
 		}
 
 		// we dont know which socket is ready so we need to poll
-		for (int i = 0; i < n_inputs; i++)
+		for (i = 0; i < n_inputs; i++)
 		{
 			if (FD_ISSET(fds[i], &readset))
 			{
@@ -270,52 +399,16 @@ int router_demon_start(void)
 			remove_pid(router_id);
 			exit(-1);
 		}
+		printf("recv: [%s]\n", buffer);
 
-		// if one connection take too long to process, we
-		// shall fork a child process to deal with receiving messages
-		if ((pid = fork()) < 0)
+		// start decoding
+		RIPPacket p;
+
+		ret = decode_message(buffer, &p);
+
+		if (ret == RIP_RESPONSE)
 		{
-			perror("Failed to fork");
-			remove_pid(router_id);
-			exit(-1);
-		}
-		else if (pid == 0)
-		{
-
-			ret = process_message(buffer);
-			if (ret == RIP_REQUEST)
-			{
-				int send_size;
-				int sender_port;
-
-				if ((sender_port = get_sender(buffer)) < 0)
-				{
-					printf("Unable to find sender\n");
-					exit(-1);
-				}
-
-				send_size = make_response(buffer);
-
-				if ((ret = send_response(buffer, send_size, sender_port)) != 0)
-				{
-					perror("Failed to send");
-					exit(-1);
-				}
-
-
-			}
-			else if (ret == RIP_RESPONSE)
-			{
-				update_table(buffer);
-			}
-
-
-			// close(client_fd);
-			exit(0);
-		}
-		else
-		{
-			// close(client_fd);
+			update_table(&p);
 		}
 
 	}
