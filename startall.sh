c9b69ba498fbdf9e939c278ddfe92562e25693ac
Ran Bao <worksev@gmail.com>
2015-05-13 14:07:24 +1200

    misc

--- startall.sh
+++ startall.sh
@@ -8,11 +8,11 @@ echo "" > log/router5.log
 echo "" > log/router6.log
 echo "" > log/router7.log
 
-./router.out start routers/router1.cfg >> log/router1.log
-./router.out start routers/router2.cfg >> log/router2.log
-./router.out start routers/router3.cfg >> log/router3.log
-./router.out start routers/router4.cfg >> log/router4.log
-./router.out start routers/router5.cfg >> log/router5.log
-./router.out start routers/router6.cfg >> log/router6.log
-./router.out start routers/router7.cfg >> log/router7.log
+./router.out start routers/normal/router1.cfg >> log/router1.log
+./router.out start routers/normal/router2.cfg >> log/router2.log
+./router.out start routers/normal/router3.cfg >> log/router3.log
+./router.out start routers/normal/router4.cfg >> log/router4.log
+./router.out start routers/nornal/router5.cfg >> log/router5.log
+./router.out start routers/normal/router6.cfg >> log/router6.log
+./router.out start routers/normal/router7.cfg >> log/router7.log
 
